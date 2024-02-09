17a7efd60d6904e7afb985bbcc853e1f6ff746c9
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-02-09 15:55:26 +0100

    Wachtwoord input aangepast

--- accessibility_hub/administrators/forms.py
+++ accessibility_hub/administrators/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from .models import Medewerker
 
 class CreateEmployeeForm(forms.ModelForm):
-
+    wachtwoord = forms.CharField(label='wachtwoord', widget = forms.PasswordInput)
     class Meta:
 
         model = Medewerker
--- accessibility_hub/administrators/models.py
+++ accessibility_hub/administrators/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from django.contrib.auth.hashers import make_password
 
 
