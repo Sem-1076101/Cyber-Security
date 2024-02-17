2fa8df53e5525ab38f62c04e4695b7bf601a5c1c
Bryan de Knikker <1065913@hr.nl>
2024-02-17 18:27:19 +0100

    Added changes in admin panel to add Question and Research question

--- accessibility_hub/companies/admin.py
+++ accessibility_hub/companies/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Onderzoek, Organisatie
+from .models import Onderzoek, Organisatie, Vraag, Onderzoekvraag
 
 # Register your models here.
 
 admin.site.register(Onderzoek)
 admin.site.register(Organisatie)
+admin.site.register(Vraag)
+admin.site.register(Onderzoekvraag)
