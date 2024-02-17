03d990fdc232c4802b6f8b542a7fd1de79064daa
Bryan de Knikker <1065913@hr.nl>
2024-02-17 18:28:08 +0100

    Changed for serializers to add question and research question

--- accessibility_hub/companies/serializers.py
+++ accessibility_hub/companies/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import Onderzoek
+from .models import Vraag
+from .models import Onderzoekvraag
 
 class OnderzoekSerializer(serializers.ModelSerializer):
     class Meta:
         model = Onderzoek
+        fields = '__all__'
+
+
+class VraagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vraag
+        fields = '__all__'
+
+
+class OnderzoekvraagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Onderzoekvraag
         fields = '__all__'
