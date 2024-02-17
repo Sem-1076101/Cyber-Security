d8f52f5673cd29b3b6852414704e5995359ced3d
Bryan de Knikker <1065913@hr.nl>
2024-02-17 18:28:47 +0100

    Added URLS for the questions

--- accessibility_hub/companies/urls.py
+++ accessibility_hub/companies/urls.py
@@ -7,6 +7,8 @@ app_name = 'companies'
 urlpatterns = [
     path('onderzoek/', views.onderzoek_lijst, name='onderzoek_lijst'),
     path('onderzoek/<int:onderzoek_id>', views.onderzoek_detail, name='onderzoek_detail'),
+    path('vraag/', views.vragen_lijst, name='vragen_lijst'),
+    path('onderzoeksvraag/', views.onderzoek_vragen_lijst, name='onderzoek_vragen_lijst'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
