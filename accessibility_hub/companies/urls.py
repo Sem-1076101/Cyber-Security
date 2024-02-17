3cb64ea273df8a8c2f7e1d19a67aee2530a0bb6b
Bryan de Knikker <1065913@hr.nl>
2024-02-17 14:49:11 +0100

    Added URLS for researches

--- accessibility_hub/companies/urls.py
+++ accessibility_hub/companies/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'companies'
 
 urlpatterns = [
+    path('onderzoek/', views.onderzoek_lijst, name='onderzoek_lijst'),
+    path('onderzoek/<int:onderzoek_id>', views.onderzoek_detail, name='onderzoek_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
