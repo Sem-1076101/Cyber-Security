1310336050213fbb470385543e513746cbab9c85
Bryan de Knikker <1065913@hr.nl>
2024-02-18 10:53:45 +0100

    Changed serializer

--- accessibility_hub/companies/serializers.py
+++ accessibility_hub/companies/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Onderzoek
 from .models import Vraag
 from .models import Onderzoekvraag
+from django.contrib.auth.models import User
 
 class OnderzoekSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +20,19 @@ class OnderzoekvraagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Onderzoekvraag
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
