3685ffd91ea11a00bc62a3341d8c25e13994af1d
Bryan de Knikker <1065913@hr.nl>
2024-02-18 10:53:14 +0100

    Added urls for login and signup for companies

--- accessibility_hub/companies/urls.py
+++ accessibility_hub/companies/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -9,6 +9,9 @@ urlpatterns = [
     path('onderzoek/<int:onderzoek_id>', views.onderzoek_detail, name='onderzoek_detail'),
     path('vraag/', views.vragen_lijst, name='vragen_lijst'),
     path('onderzoeksvraag/', views.onderzoek_vragen_lijst, name='onderzoek_vragen_lijst'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('test_token/', views.test_token, name='test token'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
