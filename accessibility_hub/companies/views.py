e1290c1b016e9dd17114cb95f0a1c343ce1331c8
Bryan de Knikker <1065913@hr.nl>
2024-02-18 10:53:04 +0100

    Added views for login and signup for companies

--- accessibility_hub/companies/views.py
+++ accessibility_hub/companies/views.py
@@ -4,7 +4,10 @@ from .models import Onderzoekvraag
 from .serializers import OnderzoekSerializer
 from .serializers import VraagSerializer
 from .serializers import OnderzoekvraagSerializer
+from .serializers import UserSerializer
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -62,3 +65,27 @@ def onderzoek_vragen_lijst(request, format=None):
         onderzoekvraag = Onderzoekvraag.objects.all()
         serializer = OnderzoekvraagSerializer(onderzoekvraag, many=True)
         return Response({'onderzoekvragen': serializer.data})
+
+
+@api_view(['POST'])
+def login(request):
+    return Response({})
+
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=serializer.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def test_token(request):
+    return Response({})
