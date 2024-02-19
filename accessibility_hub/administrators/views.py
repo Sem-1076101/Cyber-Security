c2c0f77d6db3a5653b5dc5d925e3ad838ee17246
Bryan de Knikker <1065913@hr.nl>
2024-02-19 15:40:24 +0100

    Chnaged the views

--- accessibility_hub/administrators/views.py
+++ accessibility_hub/administrators/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.urls import reverse
-from .models import Onderzoek, Medewerker, Organisatie, Ervaringsdeskundige, Beperking
+# from .models import Onderzoek, Medewerker, Organisatie, Ervaringsdeskundige, Beperking
+from .models import Medewerker, Ervaringsdeskundige, Beperking
 
 
 def login(request):
@@ -14,13 +15,14 @@ def signup(request):
 
 
 def portal(request):
-    onderzoeken = Onderzoek.objects.all()[:5]
+    # onderzoeken = Onderzoek.objects.all()[:5]
     medewerkers = Medewerker.objects.all()
-    organisaties = Organisatie.objects.all()
+    # organisaties = Organisatie.objects.all()
     ervaringsdeskundigen = Ervaringsdeskundige.objects.all()
     beperkingen = Beperking.objects.all()
-    return render(request, 'portal.html', {'onderzoeken': onderzoeken, 'medewerkers': medewerkers,
-                                              'organisaties': organisaties, 'ervaringsdeskundigen': ervaringsdeskundigen, 'beperkingen': beperkingen})
+    # return render(request, 'portal.html', {'onderzoeken': onderzoeken, 'medewerkers': medewerkers,
+    #                                           'organisaties': organisaties, 'ervaringsdeskundigen': ervaringsdeskundigen, 'beperkingen': beperkingen})
+    return render(request, 'portal.html', {'medewerkers': medewerkers, 'ervaringsdeskundigen': ervaringsdeskundigen, 'beperkingen': beperkingen})
 
 def medewerker(request, medewerker_id):
     medewerker = Medewerker.objects.get(medewerker_id=medewerker_id)
