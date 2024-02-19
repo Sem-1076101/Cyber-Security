9a8dc7d86445a681a86323079066882592967ca6
Bryan de Knikker <1065913@hr.nl>
2024-02-19 15:39:03 +0100

    Added organisation to database

---
+++ accessibility_hub/companies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class Organisatie(AbstractUser):
+    bedrijfsnaam = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True, db_column='email')
+    token = models.CharField(max_length=255, unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = 'organisatie'
