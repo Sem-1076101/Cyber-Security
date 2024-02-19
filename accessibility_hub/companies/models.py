3355006d4f852c9ccb6038e3420999c731fef585
Bryan de Knikker <1065913@hr.nl>
2024-02-19 18:13:12 +0100

    Changed table name to organizations

--- accessibility_hub/companies/models.py
+++ accessibility_hub/companies/models.py
@@ -8,10 +8,11 @@ class Organisatie(AbstractUser):
     bedrijfsnaam = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True, db_column='email')
     token = models.CharField(max_length=255, unique=True)
+    status = models.CharField(max_length=255, default='Nieuw')
     username = None
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
-        db_table = 'organisatie'
+        db_table = 'organisaties'
