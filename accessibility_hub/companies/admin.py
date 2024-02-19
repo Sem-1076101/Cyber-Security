a1e19e18a094854b68a20d9d94cf463083f4fd34
Bryan de Knikker <1065913@hr.nl>
2024-02-19 15:37:56 +0100

    Deleted admin databases

--- accessibility_hub/companies/admin.py
+++ accessibility_hub/companies/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from .models import Onderzoek, Organisatie, Vraag, Onderzoekvraag
 
 # Register your models here.
 
-admin.site.register(Onderzoek)
-admin.site.register(Organisatie)
-admin.site.register(Vraag)
-admin.site.register(Onderzoekvraag)
