0a208f5868499a523206508bb1ab3353be82df0a
Bryan de Knikker <1065913@hr.nl>
2024-02-24 14:35:59 +0100

    Updated URLS with new URLS for researches

--- accessibility_hub/administrators/urls.py
+++ accessibility_hub/administrators/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
     path('organisatie/<int:id>/', views.organisatie, name='organisatie'),
     path('verwijder_organisatie/<int:id>/', views.verwijder_organisatie, name='verwijder_organisatie'),
     path('accepteer_organisatie/<int:id>/', views.accepteer_organisatie, name='accepteer_organisatie'),
+    path('updates_organisatie/', views.check_updates_organisaties, name='updates_organisatie'),
     path('mail/', views.mail, name='mail'),
-    path('updates/', views.check_updates, name='updates'),
+    path('onderzoek/<int:onderzoek_id>/', views.onderzoek, name='onderzoek'),
+    path('verwijder_onderzoek/<int:onderzoek_id>/', views.verwijder_onderzoek, name='verwijder_onderzoek'),
+    path('accepteer_onderzoek/<int:onderzoek_id>/', views.accepteer_onderzoek, name='accepteer_onderzoek'),
+    path('updates_onderzoek/', views.check_updates_onderzoek, name='updates_onderzoek'),
 ]
