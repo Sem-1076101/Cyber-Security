dbd720984d758b11f6f1f9dadb3b75b22fa44fdc
Bryan de Knikker <1065913@hr.nl>
2024-02-24 12:27:48 +0100

    Added url for adding research

--- accessibility_hub/companies/urls.py
+++ accessibility_hub/companies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Register, Login
+from .views import Register, Login, Research
 
 app_name = 'companies'
 
 urlpatterns = [
     path('register/', Register.as_view()),
     path('login/', Login.as_view()),
+    path('onderzoek/', Research.as_view()),
 ]
