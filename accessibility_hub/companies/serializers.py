9e15d2599ff080389637c974bdce5cb56cc0cd41
Bryan de Knikker <1065913@hr.nl>
2024-02-24 12:27:33 +0100

    New serializer with research added so organisations can add a research with their token as validation

--- accessibility_hub/companies/serializers.py
+++ accessibility_hub/companies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Organisatie
+from .models import Organisatie, Onderzoek
 
 
 class OrganisatieSerializer(serializers.ModelSerializer):
@@ -20,4 +20,50 @@ class OrganisatieSerializer(serializers.ModelSerializer):
         if wachtwoord is not None:
             instance.set_password(wachtwoord)
         instance.save()
+        return instance
+
+
+class OnderzoekSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(write_only=True)  # Voeg het token-veld toe
+
+    class Meta:
+        model = Onderzoek
+        fields = ['onderzoek_id', 'titel', 'beschikbaar', 'beschrijving', 'startdatum', 'einddatum', 'type_onderzoek',
+                  'locatie', 'met_beloning', 'beloning', 'doelgroep_leeftijd_van', 'doelgroep_leeftijd_tot',
+                  'doelgroep_beperking', 'token']  # Verwijder 'organisatie' uit de lijst met velden
+        extra_kwargs = {
+            'titel': {'validators': []},  # Schakel standaard validatoren uit
+        }
+
+    def validate(self, data):
+        # Controleer of de titel uniek is binnen de context van de huidige organisatie
+        token = data.get('token')
+        if token is None:
+            raise serializers.ValidationError("Geen API-token verstrekt.")
+
+        try:
+            organisatie = Organisatie.objects.get(token=token)  # Zoek de organisatie met de opgegeven token
+        except Organisatie.DoesNotExist:
+            raise serializers.ValidationError("Ongeldige API-token.")
+
+        titel = data.get('titel')
+        if Onderzoek.objects.filter(organisatie=organisatie, titel=titel).exists():
+            raise serializers.ValidationError(
+                "Deze titel wordt al gebruikt voor een ander onderzoek van uw organisatie.")
+
+        return data
+
+    def create(self, validated_data):
+        token = validated_data.pop('token', None)  # Haal de token uit de gevalideerde gegevens
+
+        try:
+            organisatie = Organisatie.objects.get(token=token)  # Zoek de organisatie met de opgegeven token
+        except Organisatie.DoesNotExist:
+            raise serializers.ValidationError("Ongeldige API-token.")
+
+        validated_data['organisatie'] = organisatie
+
+        instance = Onderzoek.objects.create(**validated_data)
+
+        print("Organisatie ID:", organisatie.id)
         return instance
