6dccffce923cc69efcc86fa83903082308bc3a16
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-03-06 14:18:48 +0100

    Je kan nu inloggen en registreren als ervaringsdeskundige. Ook al begin homepage voor de deskundige

--- accessibility_hub/experiential_experts/forms.py
+++ accessibility_hub/experiential_experts/forms.py
@@ -1,11 +1,15 @@
-from .models import Onderzoek, Medewerker, Organisatie, Ervaringsdeskundige, Beperking
-
-# Authenticatie imports voor de login
-from django.contrib.auth.models import auth
-from django.contrib.auth import authenticate, login, logout 
-
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Medewerker
+from administrators.models import Ervaringsdeskundige
 from django.forms.widgets import PasswordInput, TextInput
+from django.core.exceptions import ValidationError
+
+class CreateExpertForm(forms.Form):
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
+
+class LoginFormExpert(forms.Form):
+    email = forms.CharField(label='Email', max_length=100)
+    wachtwoord = forms.CharField(label='wachtwoord', widget = forms.PasswordInput)
