931d5e9288355084595e53144c607fc4a894a791
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-03-18 17:26:42 +0100

    Nu laat ajax elk acc 1 keer zien

--- accessibility_hub/static/js/portal_deskundige_ajax.js
+++ accessibility_hub/static/js/portal_deskundige_ajax.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
+    var deskundigeIds = []; // Lijst van deskundige die toegevoegd zijn om te checken dat hij ze niet nog een keer toevoegd.
+
     function getRecentErvaringsdeskundigen() {
         $.ajax({
+            // standaard ajax procedure
             url: '/medewerkers/get_deskundige_in_behandeling_ajax',
             type: 'GET',
             dataType: 'json',
             success: function(data) {
                 $.each(data.ervaringsdeskundigen, function(index, ervaringsdeskundige){
                     if (ervaringsdeskundige.account_status === 0) { // Alleen account_status 0 (in behandeling)
-                        var row = '<tr class="clickable-row" data-href="/medewerkers/ervaringsdeskundige/' + ervaringsdeskundige.deskundige_id + '">' +
-                                      '<td>' + ervaringsdeskundige.voornaam + ' ' + ervaringsdeskundige.achternaam + '</td>' +
-                                      '<td>' + ervaringsdeskundige.geboortedatum + '</td>' +
-                                      '<td>' + ervaringsdeskundige.email + '</td>' +
-                                      '<td>' + ervaringsdeskundige.telefoonnummer + '</td>' +
-                                      '<td>' + ervaringsdeskundige.soort_beperking + '</td>' +
-                                      '<td>' + getStatus(ervaringsdeskundige.account_status) + '</td>' +
-                                      '<td>' + ervaringsdeskundige.created_at + '</td>' +
-                                  '</tr>';
-                        $('#recent-ervaringsdeskundigen-table').append(row); // Voeg de rij toe aan de tabel
+                        // Controleer of de deskundige al is toegevoegd aan de tabel
+                        if (!deskundigeIds.includes(ervaringsdeskundige.deskundige_id)) {
+                            var row = '<tr class="clickable-row" data-href="/medewerkers/ervaringsdeskundige/' + ervaringsdeskundige.deskundige_id + '">' +
+                                          '<td>' + ervaringsdeskundige.voornaam + ' ' + ervaringsdeskundige.achternaam + '</td>' +
+                                          '<td>' + ervaringsdeskundige.geboortedatum + '</td>' +
+                                          '<td>' + ervaringsdeskundige.email + '</td>' +
+                                          '<td>' + ervaringsdeskundige.telefoonnummer + '</td>' +
+                                          '<td>' + ervaringsdeskundige.soort_beperking + '</td>' +
+                                          '<td>' + getStatus(ervaringsdeskundige.account_status) + '</td>' +
+                                          '<td>' + ervaringsdeskundige.created_at + '</td>' +
+                                      '</tr>';
+                            $('#recent-ervaringsdeskundigen-table').append(row); // Voeg de rij toe aan de tabel
+                            deskundigeIds.push(ervaringsdeskundige.deskundige_id); // Voeg de ID toe aan de lijst
+                        }
                     }
                 });
             },
