42896769685c545a422ffe7c6dd84076be50c1bd
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-04-09 13:20:25 +0200

    Leeftijds controle bij aanmelden deskundige voor toezichthouder/voogd

--- accessibility_hub/static/js/signup.js
+++ accessibility_hub/static/js/signup.js
@@ -9,4 +9,30 @@ $(document).ready(function() {
             $("#supervisor_div").fadeOut();
         }
     });
+
+    $("#birthday").change(function() {
+        var geboortedatum = new Date($(this).val());
+        var vandaag = new Date();
+        var leeftijd = vandaag.getFullYear() - geboortedatum.getFullYear();
+        var maandVerschil = vandaag.getMonth() - geboortedatum.getMonth();
+        
+        if (maandVerschil < 0 || (maandVerschil === 0 && vandaag.getDate() < geboortedatum.getDate())) {
+            age--;
+        }
+        
+        if (leeftijd < 18) {
+            $("#supervisor_div").fadeIn();
+            $("#supervisor_div_btn_hide").fadeOut();
+            $("#supervisorName").prop("required", true);
+            $("#email_supervisor").prop("required", true);
+            $("#phonenumber_supervisor").prop("required", true);
+            
+        } else {
+            $("#supervisor_div").fadeOut();
+            $("#supervisor_div_btn_hide").fadeIn();
+            $("#supervisorName").prop("required", false);
+            $("#email_supervisor").prop("required", false);
+            $("#phonenumber_supervisor").prop("required", false);
+        }
+    });
 });
